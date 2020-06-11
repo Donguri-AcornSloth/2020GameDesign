ea8787c9dd82baa760df40c5321820ebcefd1881
DnGriDesktop <m0118288dc@edu.teu.ac.jp>
2020-06-11 17:01:09 +0900

    PlayerAudioを追加（失敗）

---
+++ Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+    public GameObject Particle;
+    public AudioSource destroyAudio;
+    ParticleSystem PS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PS = Particle.GetComponent<ParticleSystem>();
+        destroyAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")				 //　当たった相手のTagが”Wall”ならば・・
+        {
+            this.gameObject.SetActive(false);
+            Instantiate(Particle, this.gameObject.transform.position, this.gameObject.transform.rotation, null);
+            PS.Play();
+            destroyAudio.Play();
+            //　Invoke（○○秒後に他のメソッドに飛ばす関数）、2秒後に「ObjActive」に飛びます
+        }
+    }
+}
